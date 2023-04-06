2f90c356eb8876587a9ae1fbe29881d9bc18a53a
Clavi <Clavi@DESKTOP-2O92DCN.mynetworksettings.com>
2023-04-06 19:28:35 -0400

    Batch9 Github5

---
+++ src/HwDemo5/HwTest5.java
@@ -0,0 +1,12 @@
+package HwDemo5;
+
+public class HwTest5 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("HW project batch 9");
+		
+		System.out.println("Project 5");
+	}
+
+}
